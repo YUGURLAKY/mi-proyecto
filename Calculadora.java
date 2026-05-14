69bfdfeb387219f6bd26b4903e548f9709f877e6
brayan <estabancossio1404@gmail.com>
2026-05-13 20:31:31 -0500

    feat: agregar función multiplicar

--- Calculadora.java
+++ Calculadora.java
@@ -4,10 +4,16 @@ public class Calculadora {
         return a + b;
     }
 
+    public static int multiplicar(int a, int b) {
+        return a * b;
+    }
+
     public static void main(String[] args) {
 
-        int resultado = sumar(5, 3);
+        int suma = sumar(5, 3);
+        int multiplicacion = multiplicar(5, 3);
 
-        System.out.println("Resultado: " + resultado);
+        System.out.println("Suma: " + suma);
+        System.out.println("Multiplicación: " + multiplicacion);
     }
 }
